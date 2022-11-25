81165400ef57c9a88c9e23a22f2ec3fb5b7be9ec
mohamedmosilhy <ms371352@gmail.com>
2022-11-25 19:11:06 +0200

    changes

--- valid-parentheses-problem.cpp
+++ valid-parentheses-problem.cpp
@@ -3,48 +3,73 @@ using namespace std;
 #include"stack"
 #include<string>
 
+// bool isValid(string s){
+//     stack <char> st;
+//     for(auto i : s){
+//         if (st.empty())
+//         {
+//            st.push(i);
+//         }
+//         else if (i == '[' ||i == '{' ||i == '(')
+//         {
+//             st.push(i);
+//         }
+//         else if (i == ']'){ 
+//             if (st.top() == '[')
+//             {
+//                 st.pop();
+//             }
+//             else
+//             break;   
+//         } 
+//         else if (i == '}'){ 
+//             if (st.top() == '{')
+//             {
+//                 st.pop();
+//             }
+//             else
+//             break;    
+//         } 
+//         else if (i == ')'){ 
+//             if (st.top() == '(')
+//             {
+//                 st.pop();
+//             }
+//             else
+//             break;    
+//         } 
+//     }
+//     return st.empty();
+// }
+
 bool isValid(string s){
     stack <char> st;
     for(auto i : s){
         if (st.empty())
         {
-           st.push(i);
-        }
-        else if (i == '[' ||i == '{' ||i == '(')
-        {
             st.push(i);
         }
-        else if (i == ']'){ 
-            if (st.top() == '[')
+        else if(i == '[' ||i == '{' ||i == '('){
+            st.push(i);
+        }
+        else if(i == ']'||i == '}'||i == ')'){
+            if ((i == ']' && st.top() == '[') ||(i == '}' && st.top() == '{') ||(i == ')' && st.top() == '('))
             {
                 st.pop();
             }
             else
-            break;   
-        } 
-        else if (i == '}'){ 
-            if (st.top() == '{')
             {
-                st.pop();
+                return false;
             }
-            else
-            break;    
-        } 
-        else if (i == ')'){ 
-            if (st.top() == '(')
-            {
-                st.pop();
-            }
-            else
-            break;    
-        } 
+        }
     }
     return st.empty();
 }
 
+
 int main(){
 
-    cout<<isValid("(])");
+    cout<<isValid("]");
 
     return 0;
 }
